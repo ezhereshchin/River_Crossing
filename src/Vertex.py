8b13c69257e48efa3c3d940ffff223713639e0cc
zher0340 <zher0340@N2085-24-1.AD.WLU.CA>
2018-10-04 15:32:28 -0400

    added boat

--- src/Vertex.py
+++ src/Vertex.py
@@ -1,27 +1,33 @@
 
 class Vertex():
 
-    def __init__(self,left_mis,left_can,left_mis,right_mis,right_can):
-        self.left = Left(left_mis,left_can)
-        self.right = Right(right_mis,right_can)
+    def __init__(self,left_mis,left_can,left_mis,right_mis,right_can, boat_on_left):
+       
+        if boat_on_left:
+            self.left = Left(left_mis,left_can,True)
+            self.right = Right(right_mis,right_can,False)
+        else:
+             self.left = Left(left_mis,left_can,False)
+             self.right = Right(right_mis,right_can,True)
         return
     
     
 class Left():
     canibals = 0
     missionaries = 0
+    boat = False    
     
-    def __init__(self,mis,can):
+    def __init__(self,mis,can,boat):
         self.canibals = can
         self.missionaries = mis
+        self.boat=True
         return
     
 class Right():
     canibals = 0
     missionaries = 0
-    
-    def __init__(self,mis,can):
+    boat = False
+    def __init__(self,mis,can,boat):
         self.canibals = can
         self.missionaries = mis
         return
-    
